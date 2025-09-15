25a7a1992dca5c7c32f8ed982379dcaa772499d2
ukuim <117264208+ukuim@users.noreply.github.com>
2025-09-15 12:30:54 +0800

    Update date.py

--- date.py
+++ date.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 import pytz
 beijing_timezone = pytz.timezone('Asia/Shanghai')
-daaaaa=datetime(2025, 9,13, tzinfo=beijing_timezone)
+daaaaa=datetime(2025, 9,25, tzinfo=beijing_timezone)
